73197345475a46280b68367b7f7c486102baa6e2
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-07-18 13:26:29 -0500

    add cart functionality w api

---
+++ e-commerce-app/src/components/Cart/Cart.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
+
+  const fetchCartItems = async () => {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/carts/1');
+      setCartItems(response.data.products);
+    } catch (error) {
+      console.error('Error fetching cart items:', error);
+    }
+  };
+
+  const calculateTotalPrice = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    return total;
+  };
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <div>
+          {cartItems.map((item) => (
+            <div key={item.id}>
+              <h3>{item.title}</h3>
+              <p>Price: ${item.price}</p>
+              <p>Quantity: {item.quantity}</p>
+            </div>
+          ))}
+          <h4>Total Price: ${calculateTotalPrice()}</h4>
+          <button>Checkout</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
