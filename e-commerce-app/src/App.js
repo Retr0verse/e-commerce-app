f7055afa51e172d3798af43cd37f85b16c5ecd02
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-07-20 14:41:09 -0500

    troubleshoot homepage section not import to app js

--- e-commerce-app/src/App.js
+++ e-commerce-app/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Homepage from './pages/Homepage';
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
---
+++ e-commerce-app/src/pages/HomePage/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ProductCard from './ProductCard'; // Assuming you have a ProductCard component to render individual products
+import ProductsData from './ProductsData'; // Assuming you have a component to fetch products data
+
+const Homepage = () => {
+  const products = ProductsData(); // Fetch products data from the backend API
+
+  return (
+    <div>
+      <h1>Welcome to Our E-Commerce Store!</h1>
+      <div className="product-grid">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
