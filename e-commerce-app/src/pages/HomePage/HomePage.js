20945b42ac4130c701a3e47f0b334cd40cc81f7c
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-08-04 18:36:58 -0500

    add hero picture with shop now link in center

--- e-commerce-app/src/pages/HomePage/HomePage.js
+++ e-commerce-app/src/pages/HomePage/HomePage.js
@@ -1,32 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import ProductCard from '../../components/ProductCard/ProductCard';
-import { fetchProductsByCategory } from '../../utils/api'; // Assuming you have a function fetchProductsByCategory
+import React from 'react';
 import './HomePage.css';
 
 const HomePage = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetchProducts(); // Fetch products from the default category (e.g., "women's clothing")
-
-  }, []);
-
-  const fetchProducts = async () => {
-    try {
-      const data = await fetchProductsByCategory("women's clothing");
-      setProducts(data);
-    } catch (error) {
-      console.error('Error fetching products:', error);
-    }
-  };
-
   return (
     <div className="homepage-container">
       <div className="hero-section">
-        <h1>Welcome to Fashion Emporium!</h1>
-        <p>Shop the latest trends in women's clothing and jewelry.</p>
+        <div className="hero-content">
+          <h1>Welcome to Fashion Emporium!</h1>
+          <p>Shop the latest trends in women's clothing and jewelry.</p>
+          <a href="/products" className="shop-now-link">Shop Now</a>
+        </div>
       </div>
-
+      <div className="product-grid">
+        {/* No products to display */}
+      </div>
     </div>
   );
 };
