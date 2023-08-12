8987ab97f9459c42da8b4bfca324a2f9bc9a0ea9
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-08-11 19:12:03 -0500

    update jewelry page w visible cart changes

--- e-commerce-app/src/pages/JewelryProductPage/JewelryProductPage.js
+++ e-commerce-app/src/pages/JewelryProductPage/JewelryProductPage.js
@@ -1,43 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import ProductCard from '../../components/ProductCard/ProductCard';
 import axios from 'axios';
+import ProductCard from '../../components/ProductCard/ProductCard';
+import Cart from '../../components/Cart/Cart'; // Make sure to import Cart component
 import './JewelryProductPage.css';
 
 const JewelryProductPage = () => {
-  const [products, setProducts] = useState([]);
+  const [jewelryProducts, setJewelryProducts] = useState([]);
   const [cartItems, setCartItems] = useState([]); // Add cartItems state
 
   useEffect(() => {
-    fetchProductsByCategory('jewelery');
-
+    fetchProductsByCategory("jewelery");
   }, []);
 
   const fetchProductsByCategory = async (category) => {
     try {
       const response = await axios.get(`https://fakestoreapi.com/products/category/${category}`);
-      setProducts(response.data);
+      setJewelryProducts(response.data);
     } catch (error) {
       console.error('Error fetching products:', error);
     }
+  };
+
+  const addToCart = (product) => {
+    setCartItems((prevCartItems) => [...prevCartItems, { ...product, quantity: 1 }]);
   };
 
   return (
     <div className="jewelry-product-page">
       <h1>Explore Jewelry</h1>
       <div className="product-grid">
-        {products.length > 0 ? (
-          products.map((product) => (
+        {jewelryProducts.length > 0 ? (
+          jewelryProducts.map((product) => (
             <ProductCard
               key={product.id}
               product={product}
-              cartItems={cartItems}
-              setCartItems={setCartItems}
+              onAddToCart={addToCart} // Pass the addToCart function as a prop
             />
           ))
         ) : (
           <p>Loading products...</p>
         )}
       </div>
+      <Cart cartItems={cartItems} /> {/* Display the cart */}
     </div>
   );
 };
