6b0e2ca0f73cf4892547397e9ef04bf856ed10db
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-11-16 23:34:52 -0600

    revert checkoutpage to original code for now

--- e-commerce-app/src/pages/CheckoutPage/CheckoutPage.js
+++ e-commerce-app/src/pages/CheckoutPage/CheckoutPage.js
@@ -1,61 +1,11 @@
 import React from 'react';
 import './CheckoutPage.css';
-import CartItem from '../../components/CartItem/CartItem';
 
-const CheckoutPage = ({ cartItems }) => {
-  const calculateTotalPrice = () => {
-    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-
+const CheckoutPage = () => {
   return (
     <div className="checkout-page">
       <h1>Checkout</h1>
-      <div className="checkout-summary">
-        <h2>Order Summary</h2>
-        {cartItems.map((item) => (
-          <CartItem
-            key={item.id}
-            item={item}
-            removeFromCart={() => {} /* Implement the removeFromCart function if needed */}
-          />
-        ))}
-        <h4>Total Price: ${calculateTotalPrice()}</h4>
-      </div>
-      <div className="checkout-form">
-        <form>
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="address">Shipping Address:</label>
-            <textarea id="address" name="address" rows="4" required></textarea>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="creditCard">Credit Card Number:</label>
-            <input type="text" id="creditCard" name="creditCard" required />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="expiryDate">Expiry Date:</label>
-            <input type="text" id="expiryDate" name="expiryDate" placeholder="MM/YY" required />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="securityCode">Security Code:</label>
-            <input type="text" id="securityCode" name="securityCode" required />
-          </div>
-
-          <button className="checkout-button">Place Order</button>
-        </form>
-      </div>
+      {/* Your checkout form and other content */}
     </div>
   );
 };
